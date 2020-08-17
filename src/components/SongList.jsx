8c6b42ac972ef88792c32971390fbc8614245593
sineava <1690150504@qq.com>
2020-08-17 18:11:35 +0800

    🎨 歌单详情

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,42 +1,80 @@
-import React, { useState ,useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import './SongList.scss'
 import { Spin } from 'antd'
 import PubSub from 'pubsub-js'
+import BScroll from '@better-scroll/core'
+import Pullup from '@better-scroll/pull-up'
+import videoIcon from '../assets/icons/control/video-camera.png'
+import { Link } from 'react-router-dom'
 
-const SongList = ({match}) => {
-  const [songList,setSongList] = useState([])
+BScroll.use(Pullup)
+
+const SongList = ({ match }) => {
+  const wrapper = useRef(null)
+  const [songList, setSongList] = useState([])
   useEffect(() => {
-    if (match.params.id !== 'album') {
-      axios.get(`/songlist?id=${match.params.id}`)
-      .then(res => {
-        if (res.status === 200) {
-          const ids= res.data.data.songlist.map(song => [song.albummid,song.songmid]).slice(0,20)
-          setSongList(ids)
-        }
-      })
+    let arrs = []
+    const loadingData = async () => {
+      const res = await axios.get(`/qq/songlist?id=${match.params.id}`)
+      if (res.status === 200) {
+        const songs = res.data.data.songlist
+          .filter(song => song.albummid)
+          .map(song => [song.albummid, song.songmid,song.vid])
+        const arr = songs.splice(0, 20)
+        setSongList(arr)
+        arrs = arr
+        const bs = new BScroll(wrapper.current, {
+          click: true,
+          scrollY: true,
+          scrollbar: false,
+          bounce: {
+            top: false
+          },
+          mouseWheel: true,
+          pullUpLoad: {
+            threshold: -30
+          }
+        })
+        bs.on('pullingUp', function () {
+          if (songs.length !== 0) {
+            arrs = [...arrs, ...songs.splice(0, 20)]
+            setSongList([...arrs])
+            // 时间不要太长,否则会有卡顿感
+            setTimeout(() => {
+              this.finishPullUp()
+              this.refresh()
+            })
+          }
+        })
+      }
     }
-  },[match.params.id])
-  function loadMusic(songmid,albummid) {
-    PubSub.publish('play', {songmid,albummid})
+    loadingData()
+  }, [match.params.id])
+  function loadMusic(albummid, songmid,vid) {
+    PubSub.publish('play', {albummid,songmid,vid})
   }
   return (
-    <div className="songlist">
-      {songList.length === 0
-        ? <Spin size="large"/>
-        : songList.map((mid,index) => 
-        {
-          const [albummid,songmid] = mid
-          return (
-            <div key={`songId-${index}`}
-              className="album"
-              data-aos="fade-up"
-              data-aos-duration="3000"
-              data-aos-delay={`${500 * index}`}>
-              <img className="cover" src={`https://y.gtimg.cn/music/photo_new/T002R300x300M000${albummid}.jpg`} alt="cover" onClick={() => {loadMusic(songmid,albummid)}}/>
-            </div>  
-          )
-        })}
+    <div className="songlist wrapper" ref={wrapper}>
+      <ul className="content">
+        {songList.length === 0
+          ? <Spin size="large" />
+          : songList.map((mid, index) => {
+            const [albummid, songmid, vid] = mid
+            return (
+              <li key={`songId-${index}`} className="album">
+                {
+                  vid && (
+                    <Link className="link" to={`/home/mv/${vid}`}>
+                      <img className="video" src={videoIcon} alt={vid} />
+                    </Link>
+                  )
+                }
+                <img className="cover" src={`https://y.gtimg.cn/music/photo_new/T002R300x300M000${albummid}.jpg`} alt="cover" onClick={() => { loadMusic(albummid,songmid,vid) }} />
+              </li>
+            )
+          })}
+      </ul>
     </div>
   )
 }
