b124481b9cadfa4036d28df14d529e4ee50b8d46
sineava <1690150504@qq.com>
2020-08-17 18:14:18 +0800

    🎨 推荐歌单

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -1,24 +1,42 @@
 import React from 'react'
 import './PlayList.scss'
 import { Link } from 'react-router-dom'
+import playAll from '../assets/icons/control/play-all.png'
+import axios from 'axios'
+import PubSub from 'pubsub-js'
 
 const PlayList = ({playlist,match}) => {
+  const playTotal = async id => {
+    const res = await axios.get(`/qq/songlist?id=${id}`)
+    if (res.status === 200) {
+      const list = res.data.data.songlist
+        .filter(song => song.albummid)
+        .map(song => [song.albummid, song.songmid,song.vid])
+      localStorage.setItem('songlist', JSON.stringify(list))
+      // 发布
+      PubSub.publish('play', {})
+    }
+  }
   return (
     <div className="playlist">
       <div className="desc-wrapper">
-        <span className="left">Curated Just For You</span>
-        <span className="right">View all</span>
+        <span className="left">推荐歌单</span>
       </div>
       <div className="playlist-wrapper">
-        { playlist.map((play,index) => (
-          <div key={`play-list-${index}`} className="playlist-item">
-            <div className='playlist-avatar' style={{backgroundImage: `url(${play.cover})`}}>
-              <Link to={`${match.url}/${play.content_id}`}></Link>
+        <div className="wrapper">
+          { playlist.map((play,index) => (
+            <div key={`play-list-${index}`} className="playlist-item">
+              <div className='playlist-avatar' style={{backgroundImage: `url(${play.cover})`}}>
+                <Link to={`${match.url}/songlist/${play.content_id}`}></Link>
+                <div className="btn-wrapper" onClick={() => playTotal(play.content_id)}>
+                  <img src={playAll} alt='全部播放' style={{height: '100%'}}  />
+                </div>
+              </div>
+              <span className="desc">{play.title}</span>
             </div>
-            <span className="desc">{play.title}</span>
-          </div>
-          ))
-        }
+            ))
+          }
+        </div>
       </div>
     </div>
   )
