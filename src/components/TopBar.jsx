4c621f2c3f4e3e95636b40ae56633c1d1ddc0b84
sineava <1690150504@qq.com>
2020-08-17 18:11:05 +0800

    🎨 顶部bar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import collapse from '../assets/icons/control/collapse.png'
 import './TopBar.scss'
+import downloadIcon from '../assets/icons/control/download.png'
 
-const NavBar = ({apse}) => {
+const NavBar = ({apse,songurl}) => {
   return (
     <div className="collapse">
       <img src={collapse} alt="collapse" onClick={apse}/>
+      {/* 下载必须是同源或者解决跨域,否则会打开而不是下载 */}
+      <a href={songurl.replace('http://122.226.161.16','/download')} download>
+        <img src={downloadIcon} alt="download" />
+      </a>
     </div>
   )
 }
