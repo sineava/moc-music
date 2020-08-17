9fc5f2e763c584ae7434bf487a0bcbbcc6c8610e
sineava <1690150504@qq.com>
2020-08-17 18:19:52 +0800

    🎨 样式文件

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+import 'core-js/es/map'
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/stable'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import 'antd/dist/antd.css'
+import 'normalize.css/normalize.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+// 导入mock就会使用mock请求,注释掉进行真实请求
+// import './mock/mock.js'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 )
